84328227a41133d9da61fae5896b6d42dc66e35f
goodbyebada <goodbyebada@naver.com>
2025-04-18 15:21:10 +0900

    feat(global): 공통 style 통일 위해 tailwind.config.ts 수정
    - breakpoint screen 값 수정
    - borderRadius default 12px로 통일
    - fontFamily NanumBarunGothic 폰트 추가
    - fontSize와 fontWeight 목적에 맞게 분리

--- frontEnd/tailwind.config.ts
+++ frontEnd/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
@@ -8,8 +9,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        base: ['"Pretendard"', '"Apple SD Gothic Neo"', "sans-serif"],
+      // rounded-12px로 통일
+      borderRadius: {
+        DEFAULT: "12px",
       },
       fontWeight: {
         regular: "400",
@@ -20,12 +22,12 @@ const config: Config = {
         caption: ["0.625rem", { lineHeight: "0.875rem" }],
         label: ["0.75rem", { lineHeight: "1rem" }],
         bodySm: ["0.875rem", { lineHeight: "1.25rem" }],
-        bodyBase: ["1rem", { lineHeight: "1.5rem" }],
+        body: ["1rem", { lineHeight: "1.5rem" }],
         bodyMd: ["1.125rem", { lineHeight: "1.625rem" }],
         subtitle: ["1.25rem", { lineHeight: "1.75rem" }],
         title: ["1.5rem", { lineHeight: "2rem" }],
-        hero: ["2rem", { lineHeight: "2.625rem", fontWeight: 500 }],
-        display: ["2.5rem", { lineHeight: "3.125rem", fontWeight: 900 }],
+        hero: ["2rem", { lineHeight: "2.625rem" }],
+        display: ["2.5rem", { lineHeight: "3.125rem" }],
       },
       colors: {
         "sky-blue": "#e8f0fe",
@@ -36,6 +38,16 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        display: ['"NanumBarunGothic"', ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        xs: "320px", //소형 모바일
+        sm: "425px", // 모바일 > 태블릿
+        md: "768px", // 태블릿 > 노트북
+        lg: "1024px", // 노트북 > 데스크탑
+        xl: "1280px", // 데스크탑 > 와이드
       },
     },
   },
