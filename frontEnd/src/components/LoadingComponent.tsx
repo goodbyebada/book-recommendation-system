fd432aff35fb3560e7f6aff88769a7a53ae05a45
goodbyebada <goodbyebada@naver.com>
2025-04-21 14:53:05 +0900

    refactor(loading-component): loading.module.css 스타일 tailwind로 마이그레이션

--- frontEnd/src/components/LoadingComponent.tsx
+++ frontEnd/src/components/LoadingComponent.tsx
@@ -1,13 +1,15 @@
 // Loading.tsx
 
 import React from "react";
-import styles from "@styles/loading.module.css";
 
 const LoadingComponent = () => {
   return (
-    <div className={styles.loading_container}>
-      <div className={styles.loading_spinner}></div>
-      <p className={styles.loading_text}>Loading...</p>
+    <div
+      // className={styles.loading_container}
+      className="flex h-[100vh] flex-col items-center justify-center"
+    >
+      <div className="h-12 w-12 animate-spin divide-solid rounded-full border-4 border-l-light-gray"></div>
+      <p className="mt-5 text-body">Loading...</p>
     </div>
   );
 };
